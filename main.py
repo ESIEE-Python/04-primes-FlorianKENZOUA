8124f7f65a779a6645df33c087cc66350048396d
FlorianKENZOUA <kenzoua.florian@gmail.com>
2024-09-16 13:47:17 +0000

    ex4 Florian Kenzoua

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""
+Module isprime()
+"""
+
 from math import sqrt
 
 #### Fonction secondaire
@@ -5,23 +9,44 @@ from math import sqrt
 
 def isprime(p):
 
-    # votre code ici
+    """
+    Retourne si p est premier ou non.
 
-    pass
+    Args :
+        p : valeur entière positive
+    
+    Returns :
+        isprime(p) : False or True
+    """
+
+    if p == 1 :
+        return False
+
+    if p == 4 :
+        return False
+
+    rac = int(sqrt(p)) # On optimise le nombre de diviseurs potentiels
+    for i in range(2, rac) :
+        div = p / i
+        ent = int(div)
+        if div - ent == 0 : # On regarde si le diviseur est entier ou non
+            return False
+
+    return True
+
 
 #### Fonction principale
 
 
 def main():
 
-    # vos appels à la fonction secondaire ici
+    """
+    permet l'appel de isprime()
+    """
 
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
-
-    print()
-
 
 if __name__ == "__main__":
     main()
